eceab4dc18faaad3cff6c5d06da881d83965837d
Arran Hobson Sayers <ahobsonsayers@gmail.com>
2025-07-23 20:54:22 +0100

    Add test

--- filter/predicate_test.go
+++ filter/predicate_test.go
@@ -43,3 +43,39 @@ func TestCreatedAfterPredicate(t *testing.T) {
 	match = predicate(listing)
 	require.False(t, match)
 }
+
+func TestMaxTicketPriceInclFeePredicate(t *testing.T) {
+	// Should match (total ticket incl fee price below £15)
+	predicate := MaxTicketPriceInclFee(15)
+	listing := twigots.TicketListing{
+		Event:      twigots.Event{Name: "test"},
+		NumTickets: 2,
+		TotalPriceExclFee: twigots.Price{
+			Currency: twigots.CurrencyGBP,
+			Amount:   24 * 100, // £24 - £12 per ticket excl fee
+		},
+		TwicketsFee: twigots.Price{
+			Currency: twigots.CurrencyGBP,
+			Amount:   3 * 100, // £4 - £14 per ticket incl fee
+		},
+	}
+	match := predicate(listing)
+	require.True(t, match)
+
+	// Should not match (total ticket price incl fee above £15)
+	predicate = MaxTicketPriceInclFee(15)
+	listing = twigots.TicketListing{
+		Event:      twigots.Event{Name: "test"},
+		NumTickets: 2,
+		TotalPriceExclFee: twigots.Price{
+			Currency: twigots.CurrencyGBP,
+			Amount:   28 * 100, // £28 - £14 per ticket excl fee
+		},
+		TwicketsFee: twigots.Price{
+			Currency: twigots.CurrencyGBP,
+			Amount:   3 * 100, // £4 - £16 per ticket incl fee
+		},
+	}
+	match = predicate(listing)
+	require.False(t, match)
+}
