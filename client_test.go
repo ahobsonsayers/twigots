cac3ab3554f082206b04c7d512a150bd0c02b339
Arran Hobson Sayers <ahobsonsayers@gmail.com>
2026-08-31 10:54:59 +0100

    Remove other references to flaresolverr

--- client_test.go
+++ client_test.go
@@ -59,10 +59,7 @@ func TestFetchListingsReal(t *testing.T) {
 	twicketsAPIKey := os.Getenv("TWICKETS_API_KEY")
 	require.NotEmpty(t, twicketsAPIKey, "TWICKETS_API_KEY is not set")
 
-	twicketsClient, err := twigots.NewClient(
-		twicketsAPIKey,
-		twigots.WithFlareSolverr("http://0.0.0.0:8191"),
-	)
+	twicketsClient, err := twigots.NewClient(twicketsAPIKey)
 	require.NoError(t, err)
 
 	// Fetch ticket listings
